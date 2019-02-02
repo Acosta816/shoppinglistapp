c90ccc286a97f719079f3f0a00a8e42be7bbe001
David Acosta <acosta816@gmail.com>
2019-02-02 15:58:40 -0800

    fourth commit

--- index.js
+++ index.js
@@ -4,22 +4,35 @@ $(function(){
       
       event.preventDefault();
       
-        const newItem = $('.js-shopping-list-entry').val();
-        $('.js-shopping-list-entry').val("");
+        const newItem = $('#js-shopping-list-entry').val();
+        $('#js-shopping-list-entry').val("");
       
-      $('.shopping-list').append(
-          `<li>
-            <span class="shopping-item">${newItem}</span>
-            <div class="shopping-item-controls">
-              <button class="shopping-item-toggle">
-                <span class="button-label">check</span>
-              </button>
-              <button class="shopping-item-delete">
-                <span class="button-label">delete</span>
-              </button>
-            </div>
-          </li>`);
+
+
+        if(newItem === ""){
+            alert("Please enter an item");
+        }
+        
+        else{
+
+            $('.shopping-list').append(
+            `<li>
+                <span class="shopping-item">${newItem}</span>
+                <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+                </div>
+            </li>`);
+
+        };
+
       });
+
+    
       
        $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
     
